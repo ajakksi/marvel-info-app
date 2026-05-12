bc82f467f9ff63eae0734bbdc26882b43acf5ff7
Ksenia <a.jakksi27@gmail.com>
2026-05-12 20:12:27 +0200

    feat: add MarvelService with basic character methods

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,17 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
+import MarvelService from "./services/MarvelService";
 import "./style/style.scss";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then(res=> console.log(res));
+marvelService.getAllCharacters().then(res=> res.data.results.forEach(item => console.log(item.name)));
+
+
+root.render(
+<App />
+);
