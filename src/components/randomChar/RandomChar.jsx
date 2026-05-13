2168e4bc2a304dbafbdf8948590124ab0790b463
Ksenia <a.jakksi27@gmail.com>
2026-05-13 15:50:28 +0200

    feat: integrate MarvelService for dynamic data in RandomChar

--- src/components/randomChar/RandomChar.jsx
+++ src/components/randomChar/RandomChar.jsx
@@ -1,22 +1,62 @@
+import {Component} from 'react'
+import MarvelService from '../../services/MarvelService';
+
 import './randomChar.scss';
-import thor from '../../resources/img/thor.jpeg';
 import mjolnir from '../../resources/img/mjolnir.png';
 
-const RandomChar = () => {
-    return (
+
+class RandomChar extends Component{
+    constructor(props){
+        super(props);
+        this.updateChar();
+    }
+
+    state = {
+        char:{}
+    }
+
+    marvelService = new MarvelService();
+
+    onCharLoaded = (char) =>{
+        this.setState({char})
+    }
+
+    updateChar = () => {
+        const id = Math.floor(Math.random() * (20-1) + 1);
+        this.marvelService
+            .getCharacter(id)
+            .then(this.onCharLoaded)
+        
+        
+    }
+
+    formatDescription = (description) => {
+        if(!description){
+           return 'There is no description for this character';
+        }
+        if(description.length >200){
+           return `${description.slice(0,200)}...`;
+        }
+        return description
+    }
+
+    render(){
+        const {char : {name,description,thumbnail,homepage,wiki}} = this.state;
+        let  desc = this.formatDescription(description);
+       
+        return (
         <div className="randomchar">
             <div className="randomchar__block">
-                <img src={thor} alt="Random character" className="randomchar__img"/>
+                <img src={thumbnail} alt="Random character" className="randomchar__img"/>
                 <div className="randomchar__info">
-                    <p className="randomchar__name">Thor</p>
-                    <p className="randomchar__descr">
-                        As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...
+                    <p className="randomchar__name">{name}</p>
+                    <p className="randomchar__descr"> {desc}
                     </p>
                     <div className="randomchar__btns">
-                        <a href="#" className="button button__main">
+                        <a href={homepage} className="button button__main">
                             <div className="inner">homepage</div>
                         </a>
-                        <a href="#" className="button button__secondary">
+                        <a href={wiki} className="button button__secondary">
                             <div className="inner">Wiki</div>
                         </a>
                     </div>
@@ -37,6 +77,8 @@ const RandomChar = () => {
             </div>
         </div>
     )
+    }
+    
 }
 
 export default RandomChar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
 import "./style/style.scss";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters().then(res=> console.log(res));
-marvelService.getAllCharacters().then(res=> res.data.results.forEach(item => console.log(item.name)));
-
 
 root.render(
 <App />
