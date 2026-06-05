7fb0efceb4e7cdfb272a382802db9b9df9b17bfd
Ksenia <a.jakksi27@gmail.com>
2026-06-05 19:43:53 +0200

    feat: implement routing with react-router-dom

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,46 +1,28 @@
+import{Route,Routes} from 'react-router-dom'
+
+import { ComicsPage,MainPage } from '../pages';
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import ComicsList from '../comicsList/ComicsList'
-
-import decoration from '../../resources/img/vision.png';
-
-import { useState } from "react";
-
 
 const App = () => {
     
-    const [selectedChar, setSelectedChar] = useState(null);
-
-    const onCharSelected = (id) =>{
-        setSelectedChar(id);
-    }
-    
-
-    return (
+   return (
         <div className="app">
-            <AppHeader/>
+            <AppHeader/> 
             <main>
-                <ComicsList/>
+                <Routes> 
 
-                    <ErrorBoundary>
-                        <RandomChar/>
-                    </ErrorBoundary>
+                    <Route path='/' element={
+                         <MainPage/>
+                    } />
 
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected ={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-                </div>
+                    <Route path="/comics" element={
+                        <ComicsPage/> 
+                    } />
+
+                </Routes> 
             </main>
         </div>
-    )
+)
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
 import "./style/style.scss";
+import { BrowserRouter as Router} from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-<App />
+    <Router>
+        <App />
+    </Router>
+
 );
