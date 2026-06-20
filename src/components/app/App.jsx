aa3363b8387189c8f9ffb0631e874bea9a7d3bd8
Ksenia <a.jakksi27@gmail.com>
2026-06-20 18:04:57 +0300

    feat: add lazy loading for pages

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,13 @@
+import { lazy, Suspense } from 'react';
 import{Route,Routes} from 'react-router-dom'
 
-import { ComicsPage,MainPage,Page404,SingleComicPage } from '../pages';
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from '../spinner/Spinner';
+
+const Page404 = lazy(()=> import('../pages/404'));
+const MainPage = lazy(()=> import('../pages/MainPage'));
+const ComicsPage = lazy(()=> import('../pages/ComicsPage'));
+const SingleComicPage = lazy(()=> import('../pages/SingleComicPage'));
 
 const App = () => {
     
@@ -9,7 +15,8 @@ const App = () => {
         <div className="app">
             <AppHeader/> 
             <main>
-                <Routes> 
+               <Suspense fallback={<Spinner/>}>
+                 <Routes> 
 
                     <Route path='/' element={
                          <MainPage/>
@@ -28,6 +35,7 @@ const App = () => {
                     } />
 
                 </Routes> 
+               </Suspense>
             </main>
         </div>
 )
